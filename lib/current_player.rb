2da4a30049d13e79b16bcc8c585dc2d9c42a5fab
Sonja Parsell <hello@sparsell.com>
2020-03-12 15:42:16 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+# First player = "X" so....odd turns are "X" and even turns are "O"
+
+# Return value = number of turns that have been played
+
+def turn_count(board)
+  board.each do |played|
+    puts "play next move #{{played}}"
+  end
+end
+
+def current_player(board)
+  turn_count(board)
+  if turn_count(board) == (2 % == 0)
+    puts ""
+end
