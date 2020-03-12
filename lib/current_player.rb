a66ca5e51c64f410a98119e7eb4b00bb2a203110
Sonja Parsell <hello@sparsell.com>
2020-03-12 17:30:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,10 +12,11 @@ def turn_count(board)
   return number_of_turns
 end
 
-def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end
-end
+
+#def current_player(board)
+#  if turn_count(board) % 2 == 0
+#    "X"
+#  else
+#    "O"
+#  end
+#end
