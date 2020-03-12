deb0cbea55294b64e4f96acd8b0c149f1fd2ae4a
Sonja Parsell <hello@sparsell.com>
2020-03-12 17:38:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,12 +5,24 @@ board = Array.new(9, "")
 
 def turn_count(board)
   number_of_turns = 0
-  board.each |played|
+  board.each do |played|
     if played == "X" || played == "O"
     number_of_turns += 1
     end
   return number_of_turns
 end
+
+#def turn_count(board) 
+#  turns = 0 
+#  board.each do |turn| 
+#    if turn == “X” 
+#      turns += 1 
+#    elsif turn == “O” 
+#      turns += 1 
+#    end 
+#  end 
+#  return turns 
+#end
 
 
 #def current_player(board)
