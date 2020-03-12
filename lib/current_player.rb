fd7113abfd51191b028aa3469b30797ef5d4b523
Sonja Parsell <hello@sparsell.com>
2020-03-12 17:24:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,11 +6,7 @@ board = Array.new(9, "")
 def turn_count(board)
   number_of_turns = 0
   board.each do |played|
-    if played == "X"
-    puts " Play '0' next"
-    else
-    puts "play '0' next"
-    end
+    if played == "X" || played == "O"
     number_of_turns += 1
   end
   return number_of_turns
